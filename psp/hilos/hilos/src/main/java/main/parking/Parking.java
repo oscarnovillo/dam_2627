63c19b57175188c18acbb0be05f2fb1d4a1992a7
oscar <oscar@|a.com>
2026-09-24 11:13:02 +0200

    hilos

--- psp/hilos/hilos/src/main/java/main/parking/Parking.java
+++ psp/hilos/hilos/src/main/java/main/parking/Parking.java
@@ -3,10 +3,7 @@ package main.parking;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Parking {
@@ -14,6 +11,7 @@ public class Parking {
     public static final int PLAZAS = 10;
     public static final int NUM_COCHES= 1000;
 
+    private final CountDownLatch latch = new CountDownLatch(NUM_COCHES*2);
     private AtomicInteger contadorCochesAparcados = new AtomicInteger();
     private AtomicInteger contadorCochesSeVan = new AtomicInteger(0);
     private final Random rand = new Random();
@@ -33,9 +31,13 @@ public class Parking {
 
             try {
                 Thread.sleep(rand.nextInt(500)+500);
+                int i = 0;
+                int j=0;
+                // lo que tengo que hacer al final.
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
+
             semaphore.release();
             contadorCochesAparcados.incrementAndGet();
         }
@@ -43,6 +45,7 @@ public class Parking {
             System.out.println("no hay plazas");
             contadorCochesSeVan.incrementAndGet();
         }
+        latch.countDown();
     }
 
     public void crearCoches()
@@ -71,12 +74,17 @@ public class Parking {
 
         //con Executor
         executor.shutdown();
+//        try {
+//            executor.awaitTermination(1, TimeUnit.MINUTES);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+
         try {
-            executor.awaitTermination(1, TimeUnit.MINUTES);
+            latch.await();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-
     }
 
     public void debug() {
